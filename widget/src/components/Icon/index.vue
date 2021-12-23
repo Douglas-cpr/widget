dc47d8387b8eb55db376a07f1fd7adfd40de5329
Douglas Vinicius <douglas-cprdev@hotmail.com>
2021-12-23 09:42:07 -0300

    (config) move folders and add ci

---
+++ widget/src/components/Icon/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <component :is="name" v-bind="$props" />
+</template>
+
+<script>
+import Loading from './Loading.vue'
+import Copy from './Copy.vue'
+import ChevronDown from './ChevronDown.vue'
+import Chat from './Chat.vue'
+import Close from './Close.vue'
+import ArrowRight from './ArrowRight.vue'
+import Atention from './Atention.vue'
+import Correct from './Correct.vue'
+
+export default {
+  components: { Loading, Copy, ChevronDown, Chat, Close, ArrowRight, Atention, Correct },
+  props: {
+    name: { type: String, required: true }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ widget/src/components/Wizard/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <component
+    :is="store.currentComponent"
+    @select-feedback-type="handleSelectFeedbackType"
+    @next="next"
+    >
+  </component>
+</template>
+
+<script lang="ts">
+import useNavigation, { Navigation } from '@/hooks/navigation'
+import useStore from '@/hooks/store'
+import { setFeedbackType, StoreState } from '@/store'
+import { defineComponent } from 'vue'
+import SelectFeedbackType from './SelectFeedbackType.vue'
+import WriteAFeedback from './WriteAFeedback.vue'
+import Success from './Success.vue'
+import ErrorState from './Error.vue'
+
+interface SetupReturn {
+  store: StoreState;
+  handleSelectFeedbackType(type: string): void;
+  next: Navigation['next'];
+
+}
+
+export default defineComponent({
+  components: { SelectFeedbackType, WriteAFeedback, Success, Error: ErrorState },
+  setup (): SetupReturn {
+    const store = useStore()
+    const { next } = useNavigation()
+
+    function handleSelectFeedbackType (type: string): void {
+      setFeedbackType(type)
+    }
+
+    return {
+      store,
+      handleSelectFeedbackType,
+      next
+    }
+  }
+})
+</script>
